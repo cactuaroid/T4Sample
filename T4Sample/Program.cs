624139dafc50dcadf68f63fcf2216a4443d53711
cactuaroid <solamiredoredore@gmail.com>
2016-07-13 00:05:52 -0700

    add sample of generating message name class to get message number

--- T4Sample/Program.cs
+++ T4Sample/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using T4Sample.Message;
 
 namespace T4Sample
 {
@@ -10,6 +11,7 @@ namespace T4Sample
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("message number is " + MyMessage.FileNotFound);
         }
     }
 }
